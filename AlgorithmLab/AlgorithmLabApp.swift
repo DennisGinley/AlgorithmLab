52936d770821cc8b29645c2036e43864358271fb
Dennis Ginley <dennis.ginley@runbox.com>
2026-07-17 11:40:34 -0700

    Initial Commit

---
+++ AlgorithmLab/AlgorithmLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmLabApp.swift
+//  AlgorithmLab
+//
+//  Created by Dennis Ginley on 7/17/26.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgorithmLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlgorithmLab
+//
+//  Created by Dennis Ginley on 7/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
